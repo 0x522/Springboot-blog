6e4132a465ebfff686549f774798c0f07ef53602
Cyt <chenyuntao1997@163.com>
2020-12-23 17:45:53 +0800

    add integration test for controller

--- src/test/java/blog/integration/MyIntegrationTest.java
+++ src/test/java/blog/integration/MyIntegrationTest.java
@@ -1,10 +1,42 @@
 package blog.integration;
 
+import blog.Application;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import javax.inject.Inject;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations = "classpath:test.properties")
 public class MyIntegrationTest {
-    @Test
-    void myIntegrationTest() {
+    @Inject
+    Environment environment; //环境信息
 
+    @Test
+    void notLoggedInByDefault() throws IOException, InterruptedException {
+        //从environment拿到端口
+        String port = environment.getProperty("local.server.port");
+        System.out.println(port);
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:" + port + "/auth"))
+                .build();
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.statusCode());
+        System.out.println(response.body());
+        Assertions.assertEquals(200, response.statusCode());
+        Assertions.assertTrue(response.body().contains("用户没有登录"));
     }
 }
